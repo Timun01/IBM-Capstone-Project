45c113481e264d4b692c1993bcb35328ff389962
Timun01 <60331469+Timun01@users.noreply.github.com>
2020-01-28 21:24:13 +1100

    IBM Capstone project notebook

---
+++ IBM Capstone project.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # IBM Data Science Professional Certificate - Capstone project
+# ____________________________________________________________________________________________________
+
+# This note book is being prepared to complete the Capstone project for IBM Data Science Professional Certificate offered via Coursera.
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
